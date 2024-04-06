0bd5c6fb2b752fb0efb4bdd932ae1d15866b513f
Kashif Raza <k.raza1932000@gmail.com>
2024-04-07 00:58:25 +0530

    Update config.php

--- admin/inc/config.php
+++ admin/inc/config.php
@@ -6,7 +6,7 @@ ini_set('error_reporting', E_ALL);
 date_default_timezone_set('America/Los_Angeles');
 
 // Host Name
-$dbhost = 'localhost';
+$dbhost = '51.210.34.197';
 
 // Database Name
 $dbname = 'kriztech_krizkart';
